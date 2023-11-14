61f90313c03c554b9c839620c8d50aea1bb57140
jonathanlukass <jhonatanlukas05@gmail.com>
2023-11-14 22:12:07 +0800

    revisi

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -11,7 +11,7 @@ const Profile = () => {
               <div className="content-center">
                 <div className="cc-profile-image"><a href="#"><img src="images/joxe.jpg" alt="Image" /></a></div>
                 <div className="h2 title">Jonathan Lukas</div>
-                <p className="category text-white">Web Developer, Graphic Designer,  Photographer (AMEN) </p><a className="btn btn-primary smooth-scroll mr-2" href="#contact" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Hire Me</a><a className="btn btn-primary" href="https://drive.google.com/file/d/1akYzSBZ5gt-RDpbnrlL_9GseBu6ZXFy1/view?usp=sharing" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Download CV</a>
+                <p className="category text-white">Web Developer, Graphic Designer,  Photographer (AMEN) </p><a className="btn btn-primary smooth-scroll mr-2" href="#contact" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Hire Me</a><a className="btn btn-primary" href="https://drive.google.com/file/d/1akYzSBZ5gt-RDpbnrlL_9GseBu6ZXFy1/view?usp=sharing" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Certificate</a>
               </div>
             </div>
             <div className="section">
